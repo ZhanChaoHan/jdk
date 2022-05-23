8fe4ba19cad38430d6f925b97903a6d6181d8d73
jachs <799516423@qq.com>
2022-05-23 16:48:15 +0800

    qwes

--- j-annotation/annotation-processor-test/src/main/java/com/jachs/annotation/processor/test/ExecuteSqlProcessorTest.java
+++ j-annotation/annotation-processor-test/src/main/java/com/jachs/annotation/processor/test/ExecuteSqlProcessorTest.java
@@ -1,6 +1,7 @@
 package com.jachs.annotation.processor.test;
 
-import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.io.IOException;
 
 import com.jachs.annotation.part2.ExecuteSQL;
 import com.jachs.annotation.part3.CreateClass;
@@ -12,16 +13,13 @@ import com.jachs.annotation.part3.CreateClass;
  */
 public class ExecuteSqlProcessorTest {
 	@ExecuteSQL(connectURL = "",driverClass = "",excuteSQLText = "",passWorld = "",userName = "")
-	public BasicDataSource a(){return null;}
-	
-	@ExecuteSQL(connectURL = "",driverClass = "",excuteSQLText = "",passWorld = "",userName = "")
 	public void b(){}
 	
 	public void cje() {}
 	@CreateClass(className = "GuguBird")
 	public class d{};
 	
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		System.out.println("OK");
 	}
 }
