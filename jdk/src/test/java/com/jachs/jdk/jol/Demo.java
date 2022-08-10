9309406e4c85d34d85e41e1f62e5e418aa97a307
jachs <799516423@qq.com>
2022-08-10 10:59:31 +0800

    打印基础类型

--- jdk/src/test/java/com/jachs/jdk/jol/Demo.java
+++ jdk/src/test/java/com/jachs/jdk/jol/Demo.java
@@ -11,8 +11,14 @@ public class Demo {
 	public static void main(String[] args) {
 		People peo=new People();
 		
-		peo.setName("jack");
-		peo.setAge(12);
+		peo.setBo(true);
+		peo.setBt((byte)112);
+		peo.setCa('a');
+		peo.setDe(1.207);
+		peo.setFt(2.98f);
+		peo.setIt(22);
+		peo.setLg(1765L);
+		peo.setSg("不是基本类型看看String对象占用内存空间多少");
 		
 		System.out.println(ClassLayout.parseInstance(peo).toPrintable());
 	}
--- jdk/src/test/java/com/jachs/jdk/jol/People.java
+++ jdk/src/test/java/com/jachs/jdk/jol/People.java
@@ -6,29 +6,145 @@ package com.jachs.jdk.jol;
  *
  */
 public class People {
-	private String name;
-	private int age;
+	//8大基本类型大小
+	private boolean bo;//1 byte
+	private byte bt;//1 byte
+	private short st;//2 byte
+	private char ca;//2 byte
+	private int it;//4 byte
+	private long lg;//8 byte
+	private float ft;//4 byte
+	private double de;//8 byte
+	
+	private String sg;
+	
 	
 	
 	public People() {
 		super();
 	}
-	public People(String name, int age) {
+
+
+
+	public People(boolean bo, byte bt, short st, char ca, int it, long lg, float ft, double de, String sg) {
 		super();
-		this.name = name;
-		this.age = age;
+		this.bo = bo;
+		this.bt = bt;
+		this.st = st;
+		this.ca = ca;
+		this.it = it;
+		this.lg = lg;
+		this.ft = ft;
+		this.de = de;
+		this.sg = sg;
 	}
-	public String getName() {
-		return name;
+
+
+
+	public boolean isBo() {
+		return bo;
 	}
-	public void setName(String name) {
-		this.name = name;
+
+
+
+	public void setBo(boolean bo) {
+		this.bo = bo;
 	}
-	public int getAge() {
-		return age;
+
+
+
+	public byte getBt() {
+		return bt;
 	}
-	public void setAge(int age) {
-		this.age = age;
+
+
+
+	public void setBt(byte bt) {
+		this.bt = bt;
+	}
+
+
+
+	public short getSt() {
+		return st;
+	}
+
+
+
+	public void setSt(short st) {
+		this.st = st;
+	}
+
+
+
+	public char getCa() {
+		return ca;
+	}
+
+
+
+	public void setCa(char ca) {
+		this.ca = ca;
+	}
+
+
+
+	public int getIt() {
+		return it;
+	}
+
+
+
+	public void setIt(int it) {
+		this.it = it;
+	}
+
+
+
+	public long getLg() {
+		return lg;
+	}
+
+
+
+	public void setLg(long lg) {
+		this.lg = lg;
+	}
+
+
+
+	public float getFt() {
+		return ft;
+	}
+
+
+
+	public void setFt(float ft) {
+		this.ft = ft;
+	}
+
+
+
+	public double getDe() {
+		return de;
+	}
+
+
+
+	public void setDe(double de) {
+		this.de = de;
+	}
+
+
+
+	public String getSg() {
+		return sg;
+	}
+
+
+
+	public void setSg(String sg) {
+		this.sg = sg;
 	}
 	
 }
